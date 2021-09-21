945180d9a655f81723ad86f602521912bc3a8bbc
Girimurugan Natarajan <girimurugan.natarajan@gmail.com>
2021-09-21 20:48:27 +0530

    added

---
+++ max-subArray.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// function to find the max sum of sub array
+
+func maxSumOfSubArray(input []int) int{
+
+
+return 0
+}
+
+
+// Driver function
+func main(){
+
+	input := []int{-1, 2, 3, -4, 5}
+
+	fmt.Println(maxSumOfSubArray(input))
+    fmt.Println(math.Max(2,3))
+}
