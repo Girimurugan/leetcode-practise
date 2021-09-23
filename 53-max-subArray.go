cdf37caa202a9bcae20e7e6fa68ae452de055a43
Girimurugan Natarajan <girimurugan.natarajan@gmail.com>
2021-09-23 23:05:56 +0530

    53

--- 53-max-subArray.go
+++ 53-max-subArray.go
@@ -15,7 +15,7 @@ func maxSumOfSubArray(input []int) int{
 
 	for i := 1 ; i <len(input) ; i++{
 
-		currentSubArray = max(input[i], currentSubArray + input[i])
+		currentSubArray = max(input[i], currentSubArray + input[i]) // change this to product for Leet code problem 152
 		maxSubArray = max(maxSubArray, currentSubArray)
 
 
@@ -42,9 +42,16 @@ func main(){
 	input := []int{-2,1,-3,4,-1,2,1,-5,4}
 	input2 := []int{5,4,-1,7,8}
 	input3 := []int{1}
+	input4 := []int{2,3,-2,4}
+	input5 := []int{-2,0,-1}
+	input6 := []int{2,-5,3,1,-4,0,-10,2,8}
 
 	fmt.Println(maxSumOfSubArray(input))
 	fmt.Println(maxSumOfSubArray(input2))
 	fmt.Println(maxSumOfSubArray(input3))
+	fmt.Println(maxSumOfSubArray(input4))
+	fmt.Println(maxSumOfSubArray(input5))
+	fmt.Println(maxSumOfSubArray(input6))
+
    
 }
